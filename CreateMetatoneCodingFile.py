0c736b5b1e5beae20242ca8559f4d21939feab22
cpmpercussion <cpm@charlesmartin.com.au>
2013-06-28 11:41:29 +1000

    Added new Python scripts

---
+++ CreateMetatoneCodingFile.py
@@ -0,0 +1,54 @@
+# Metatone Video Coding File Generator
+
+from datetime import timedelta
+from datetime import datetime
+from datetime import time
+import subprocess
+import argparse
+import math
+
+parser = argparse.ArgumentParser(description='Create a CSV Coding form for Ensemble Metatone videos.')
+parser.add_argument('filename',help='Video file used to create a coding form.')
+
+args = parser.parse_args()
+
+video_filename = args.filename
+
+def getLength(filename):
+  result = subprocess.Popen(["ffprobe", filename],
+    stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+  length = [x for x in result.stdout.readlines() if "Duration" in x][0].replace(',','').replace('  ',' ').split()[1]
+  length = length.split(':')
+  hours = int(length[0])
+  minutes = int(length[1])
+  seconds = int(math.floor(float(length[2])))
+  micro = int((float(length[2]) % 1) * 1000000)
+  return time(hours,minutes,seconds,micro)
+
+
+#video_filename = '/Volumes/cpm-projectdrive/temp/Metatone 20130504 Set 2 - 4K.mov'
+
+
+if (len(video_filename)>0):
+    print("Creating form for " + video_filename + "...")
+    
+    log_filename = video_filename[:len(video_filename) -4] + '-log.CSV'
+    video_length = getLength(video_filename)
+    window_size = timedelta(seconds = 5)
+    time_stamp = datetime(1,1,1,0,0,0)
+    
+    header = 'time,charles,christina,yvonne,jonathan\n'
+    line_template = ',,,,\n'
+    
+    log_file = open(log_filename,'w')
+    log_file.write(header)
+    while (time_stamp.time() < video_length):
+        log_file.write(str(time_stamp.minute) + ":" + str(time_stamp.second) + line_template)
+        time_stamp = time_stamp + window_size
+    log_file.close()
+else:
+    print("Cannot create form.")
+
+
+
+
---
+++ Metatone Touch Processing Experiments.py
@@ -0,0 +1,91 @@
+# Metatone Touch Experiments.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+
+##### Function to calculate feature vectors 
+## (for a dataframe containing one 'device_id'
+##
+def feature_frame(frame):
+    window_size = '5s'
+    
+    
+    frame_freq = frame['device_id'].resample(window_size,how='count') 
+    frame_touchdowns = frame.ix[frame['velocity'] == 0]
+    frame_touchdowns = frame_touchdowns['velocity'].resample(window_size,how='count')
+    frame_vel = frame['velocity'].resample(window_size,how='mean')
+    frame_centroid = frame[['x_pos','y_pos']].resample(window_size,how='mean')
+    
+    
+    fframe = pd.DataFrame({'freq':frame_freq,
+        'device_id':frame['device_id'].resample(window_size,how='first')
+            .fillna(method='ffill'),
+        'touchdown_freq':frame_touchdowns.fillna(0),
+        'movement_freq':frame_freq.fillna(0),
+        'centroid_x':frame_centroid['x_pos'].fillna(-1),
+        'centroid_y':frame_centroid['y_pos'].fillna(-1),
+        'velocity':frame_vel.fillna(0)})
+    return fframe.fillna(0)
+
+##### Load some Data
+##
+##
+#processed_file = open('/Users/charles/Dropbox/Metatone/20130504/20130504-set2-proctouches.txt','r')
+processed_file = '/Users/charles/Dropbox/Metatone/20130504/MetatoneOSCLog-20130504-14h23-touches.csv'
+
+messages = pd.read_csv(processed_file, index_col="time", parse_dates=True)
+names = messages['device_id'].unique()
+
+
+
+
+## Count up the occurences of each device_id
+#print(messages["device_id"].value_counts())
+#counts = messages["device_id"].value_counts()
+#counts.plot(kind='barh',rot=0)
+
+#plt.scatter(messages["x_pos"],messages["y_pos"])
+
+#plt.scatter(messages["x_pos"],messages["y_pos"],color='k',alpha=0.3)
+
+#plt.scatter(messages["x_pos"],768-messages["y_pos"],color='k',alpha=0.05)
+#plt.title('Density of touches in 20130504 Set 2')
+#plt.axis([0,1024,0,768])
+
+## Manually making some feature vectors....
+charles_messages = messages.ix[messages['device_id'] == 'charles']
+christina_messages = messages.ix[messages['device_id'] == 'christina']
+yvonne_messages = messages.ix[messages['device_id'] == 'yvonne']
+jonathan_messages = messages.ix[messages['device_id'] == 'jonathan']
+
+charles_freq = charles_messages['device_id'].resample('2s',how='count')
+christina_freq = christina_messages['device_id'].resample('2s',how='count')
+yvonne_freq = yvonne_messages['device_id'].resample('2s',how='count')
+jonathan_freq = jonathan_messages['device_id'].resample('2s',how='count')
+
+freqs = pd.DataFrame({'charles_freq': charles_freq, 'christina_freq': christina_freq, 
+'yvonne_freq':yvonne_freq, 'jonathan_freq':jonathan_freq})
+
+totalfrequency = messages.resample('2s',how='count')
+#totalfrequency.plot(kind='line',rot=0)
+
+
+## Classification Experiment
+
+feature_vectors = pd.DataFrame()
+for n in names:
+    feature_vectors = pd.concat([feature_vectors, 
+        feature_frame(messages.ix[messages['device_id'] == n])])
+
+gnb = GaussianNB()
+
+sampler = np.random.randint(0, len(feature_vectors), size=100)
+training_vectors = feature_vectors.take(sampler)
+
+input_vectors = training_vectors[['centroid_x','centroid_y','freq','movement_freq','touchdown_freq','velocity']]
+targets = training_vectors['device_id']
+
+name_pred = gnb.fit(input_vectors, targets).predict(input_vectors)
+print "Number of mislabeled points : %d" % (targets != name_pred).sum()
+print "Accuracy: %f\%" % (targets != name_pred).sum() * 100 /len(targets)
