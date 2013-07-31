394647da89037ec7cb93120d8f6e9f9a81cd87c8
cpmpercussion <cpm@charlesmartin.com.au>
2013-07-31 18:44:34 +1000

    more details for live processor. now logs to text file

--- live processor/metatoneLiveLoggingProcessor.py
+++ live processor/metatoneLiveLoggingProcessor.py
@@ -8,21 +8,25 @@ from datetime import timedelta
 from datetime import datetime
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-ip = ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1])
+#ip = ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1])
+ip = ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][1:])
+
 
 name    = "Metatone LiveProc"
 port    = 9000
 receive_address = (ip[0], port)
 
-
 # OSC Server. there are three different types of server. 
 s = OSC.OSCServer(receive_address) # basic
 ##s = OSC.ThreadingOSCServer(receive_address) # threading
 ##s = OSC.ForkingOSCServer(receive_address) # forking
-s.addDefaultHandlers()
-# define a message-handler function for the server to call.
+#s.addDefaultHandlers()
 
+##
+## Register the Bonjour service
+##
 def register_callback(sdRef, flags, errorCode, name, regtype, domain):
     if errorCode == pybonjour.kDNSServiceErr_NoError:
         print 'Registered service:'
@@ -35,7 +39,9 @@ sdRef = pybonjour.DNSServiceRegister(name = name,
                                      port = port,
                                      callBack = register_callback)
 
-
+##
+## Setup Functions for Logging Messages
+##
 device_names = {
     '2678456D-9AE7-4DCC-A561-688A4766C325':'charles',
     '97F37307-2A95-4796-BAC9-935BF417AC42':'christina',
@@ -44,34 +50,221 @@ device_names = {
 columns = ['time','device_id','x_pos','y_pos','velocity']
 live_messages = []
 
+touch_frame = pd.DataFrame
+# columns should be time, device_id, x_pos, y_pos, velocity
+
+logging_filename = datetime.now().isoformat().replace(":","-")[:19] + "-MetatoneOSCLog.txt"
+logging_file = open(logging_filename,'w')
+
+def log_messages(message,log):
+    print(message)
+    log.append(message)
+    if (len(log) > 1000):
+        print("logging all the messages!")
+        output_messages = []
+        for m in log:
+            output_messages.append(str(m).replace("[","").replace("]","").replace("'","") + "\n")
+        # log it to disk
+        del live_messages[:]
+        logging_file.writelines(output_messages)
+        # create a new live_messages
+
+def write_log(log):
+    print("logging all the messages!")
+    output_messages = []
+    for m in log:
+        output_messages.append(str(m).replace("[","").replace("]","").replace("'","") + "\n")
+    # log it to disk
+    del live_messages[:]
+    logging_file.writelines(output_messages)
+    # create a new live_messages
+
+
+##
+## OSC Message Handling Functions
+##
 
 def touch_handler(addr, tags, stuff, source):
     if (tags == "sfff"):
-        message = [datetime.now().isoformat(),device_names[stuff[0]],stuff[1],stuff[2],stuff[3]]
-        live_messages.append(message)
-        print(message)
+        message = [datetime.now().isoformat(),"touch",device_names[stuff[0]],stuff[1],stuff[2],stuff[3]]
+        log_messages(message,live_messages)
+        
+        
+def touch_ended_handler(addr,tags,stuff,source):
+    if (tags == "s"):
+        message = [datetime.now().isoformat(),"touch/ended",device_names[stuff[0]]]
+        log_messages(message,live_messages)
+
+def switch_handler(addr,tags,stuff,source):
+    print(tags)
+    if (tags == "ssb"):
+        message = [datetime.now().isoformat(),"switch",device_names[stuff[0]],stuff[1],stuff[2]]
+        log_messages(message,live_messages)
+        
+def onlineoffline_handler(addr,tags,stuff,source):
+    print(tags)
+    if (tags == "s"):
+        message = [datetime.now().isoformat(),addr,device_names[stuff[0]]]
+        log_messages(message,live_messages)
+        
+def accel_handler(addr,tags,stuff,source):
+    if (tags == "sfff"):
+        #do nothing
+        message = [datetime.now().isoformat(),"accel",device_names[stuff[0]],stuff[1],stuff[2],stuff[3]]
+        #log_messages(message,live_messages)
+
 
 s.addMsgHandler("/metatone/touch", touch_handler) # adding our function
+s.addMsgHandler("/metatone/touch/ended", touch_ended_handler) # adding our function
+s.addMsgHandler("/metatone/switch", switch_handler) # adding our function
+s.addMsgHandler("/metatone/online", onlineoffline_handler) # adding our function
+s.addMsgHandler("/metatone/offline", onlineoffline_handler) # adding our function
+s.addMsgHandler("/metatone/accel", accel_handler) # adding our function
 
 print "Registered Callback-functions are :"
 for addr in s.getOSCAddressSpace():
     print addr
 
 # Start OSCServer
-print "\nStarting OSCServer. Use ctrl-C to quit."
-st = threading.Thread( target = s.serve_forever )
+#print "\nStarting OSCServer. Use ctrl-C to quit."
+st = threading.Thread(target = s.serve_forever)
 st.start()
 
-# finish up.    
-sdRef.close()
-s.close()
-st.join()
+##
+## Setup Classifier
+##
+##
 
+## Int values for Gesture codes.
+gesture_codes = {
+    'N': 0,
+    'FT': 1,
+    'ST': 2,
+    'FS': 3,
+    'FSA': 4,
+    'VSS': 5,
+    'BS': 6,
+    'SS': 7,
+    'C': 8,
+    '?': 9}
+
+
+## Column names in the feature vectors:
+feature_vector_columns = ['centroid_x','centroid_y','std_x','std_y','freq','movement_freq','touchdown_freq','velocity']
+
+##### Function to calculate feature vectors 
+## (for a dataframe containing one 'device_id'
+##
+def feature_frame(frame):
+    window_size = '5s'
+    
+    
+    frame_freq = frame['device_id'].resample(window_size,how='count') 
+    frame_touchdowns = frame.ix[frame['velocity'] == 0]
+    frame_touchdowns = frame_touchdowns['velocity'].resample(window_size,how='count')
+    frame_vel = frame['velocity'].resample(window_size,how='mean')
+    frame_centroid = frame[['x_pos','y_pos']].resample(window_size,how='mean')
+    frame_std = frame[['x_pos','y_pos']].resample(window_size,how='std')
+    
+    
+    fframe = pd.DataFrame({'freq':frame_freq,
+        'device_id':frame['device_id'].resample(window_size,how='first')
+            .fillna(method='ffill'),
+        'touchdown_freq':frame_touchdowns.fillna(0),
+        'movement_freq':frame_freq.fillna(0),
+        'centroid_x':frame_centroid['x_pos'].fillna(-1),
+        'centroid_y':frame_centroid['y_pos'].fillna(-1),
+        'std_x':frame_std['x_pos'].fillna(0),
+        'std_y':frame_std['y_pos'].fillna(0),
+        'velocity':frame_vel.fillna(0)})
+    return fframe.fillna(0)
+    
+##### Function to calculate feature vectors 
+## (for a dataframe containing one 'device_id'
+##
+def make_one_feature_frame(frame):
+    window_size = '5s'
+ 
+    frame_freq = frame['device_id'].resample(window_size,how='count') 
+    frame_touchdowns = frame.ix[frame['velocity'] == 0]
+    frame_touchdowns = frame_touchdowns['velocity'].resample(window_size,how='count')
+    frame_vel = frame['velocity'].resample(window_size,how='mean')
+    frame_centroid = frame[['x_pos','y_pos']].resample(window_size,how='mean')
+    frame_std = frame[['x_pos','y_pos']].resample(window_size,how='std')
+    
+    
+    fframe = pd.DataFrame({'freq':frame_freq,
+        'device_id':frame['device_id'].resample(window_size,how='first')
+            .fillna(method='ffill'),
+        'touchdown_freq':frame_touchdowns.fillna(0),
+        'movement_freq':frame_freq.fillna(0),
+        'centroid_x':frame_centroid['x_pos'].fillna(-1),
+        'centroid_y':frame_centroid['y_pos'].fillna(-1),
+        'std_x':frame_std['x_pos'].fillna(0),
+        'std_y':frame_std['y_pos'].fillna(0),
+        'velocity':frame_vel.fillna(0)})
+    return fframe.fillna(0)
+
+
+##### Load some Data
+##
+##
+processed_file = '/Users/charles/Dropbox/Metatone/20130701/OSCLog20130701-17h20m46s.txt-touches.csv'
+messages = pd.read_csv(processed_file, index_col="time", parse_dates=True)
+
+##
+## Setup Classifier with Synthetic Data 
+##
+# Setup the Data
+feature_vectors = pd.DataFrame()
+for n in names:
+    feature_vectors = pd.concat([feature_vectors, 
+        feature_frame(messages.ix[messages['device_id'] == n])])
+        
+feature_vectors = feature_vectors[feature_vector_columns].join(gesture_targets)
+feature_vectors['gesture'] = feature_vectors['gesture'].fillna('N')
+feature_vectors['gesture'] = feature_vectors['gesture'].apply(lambda x: gesture_codes[x])
+
+# take a sample of the data for training.
+sampler = np.random.randint(0, len(feature_vectors), size=len(feature_vectors))
+training_vectors = feature_vectors.take(sampler)
+input_vectors = training_vectors[feature_vector_columns]
+targets = training_vectors['gesture']
+
+
+# Train the classifier
+classifier = RandomForestClassifier(n_estimators=100, max_features=3,compute_importances=True)
+classifier = classifier.fit(input_vectors, targets)
+
+
+## For a given chunk of messages
+
+## Divide into names
+## calculate a feature frame
+
+## if no data for a name - output 0
+
+## otherwise output classifier(frame[0])
+
+
+
+
+def close_server():
+    # finish up.    
+    sdRef.close()
+    s.close()
+    st.join()
+    write_log(live_messages)
+    logging_file.close()
+
+##
+## Classification Section
+##
 # Working
 def process_messages(messages):
    return 0
    
-def calculate_featuers(messages):
+def calculate_features(messages):
     return 0
     
 def classify_sample(sample):
@@ -90,7 +283,29 @@ gesture_codes = {
     'C': 8,
     '?': 9}
 
-
-
-
-
+# Metatone Message Structure
+#
+## Touch
+## /metatone/touch sfff
+## deviceID X Y vel
+#
+## Touch Ended
+## /metatone/touch/ended s
+## deviceID
+#
+## Switch
+## /metatone/switch ssb
+## deviceID switchname switchstate
+#
+## Online
+## /metatone/online s
+## deviceID
+#
+## Offline
+## /metatone/offline s
+## deviceID
+#
+## Acceleration
+## /metatone/acceleration sfff
+## deviceID X Y Z
+#
--- markov-experiments/Markov Transition Matrix Experiment.py
+++ markov-experiments/Markov Transition Matrix Experiment.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import itertools as it
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
