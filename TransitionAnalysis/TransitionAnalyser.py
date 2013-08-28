4125d5bdd4f2422b5cbc445be00694f753eb961c
cpmpercussion <cpm@charlesmartin.com.au>
2013-08-29 00:06:49 +1000

    Lots of changes to the Transition Analyser, now has a sensible transition_matrix function to generate transition matrices for each column of an input chain. This can be used with the resample function to try different windows then adding all the columns gives the transition matrix for the whole group. Next step is to plot the group transition matrix for each window to look at the change in transitions over a performance.

--- TransitionAnalysis/TransitionAnalyser.py
+++ TransitionAnalysis/TransitionAnalyser.py
@@ -28,33 +28,37 @@ gesture_groups = {
     7 : 3,
     8 : 4,
     9 : 4}
+
+def transition_matrix(chains):
+    states_n = 5
+    #transitions = np.zeros([states_n,states_n])
+    output = []
+    for col in chains:
+        transitions = np.zeros([states_n,states_n])
+        curr_chain = chains[col].tolist()
+        curr_chain = map(int,curr_chain)
+        prev = -1
+        for s in curr_chain:
+            curr = s
+            if (prev != -1):
+                transitions[gesture_groups[curr]][gesture_groups[prev]] = transitions[gesture_groups[curr]][gesture_groups[prev]] + 1
+            prev = s
+        output.append(transitions)
+    return output
+    #return np.reshape(transitions,-1)
+
+
     
 #processed_file = '/Users/charles/Dropbox/Metatone/touch-point-performance-analysis/markov-experiments/20130427-MetatoneGesturePredictions.csv'
 #processed_file = '/Users/charles/Dropbox/Metatone/touch-point-performance-analysis/markov-experiments/20130427-MetatoneGestureTargets.csv'
 processed_file = '/Users/charles/Dropbox/Metatone/touch-point-performance-analysis/metatone-performances/20130803-18h-performance/MetatoneGestureScore20130803-18h38m10s.csv'
 
-chains = pd.read_csv(processed_file, index_col="time", parse_dates=True)
+state_data = pd.read_csv(processed_file, index_col="time", parse_dates=True)
 
-    
-states_n = 5
-transitions = np.zeros([states_n,states_n])
 
-for col in chains:
+windowed_transitions = state_data.resample('30s',how=transition_matrix)
+windowed_transitions['group'] = windowed_transitions['jonathan'] + windowed_transitions['christina'] + windowed_transitions['yvonne'] + windowed_transitions['christina']
 
-    curr_chain = chains[col].tolist()
-    curr_chain = map(int,curr_chain)
-
-    prev = -1
-    for s in curr_chain:
-        curr = s
-        #print ("Current: " + str(curr) + " Previous: " + str(prev))
-        
-        if (prev != -1):
-            # increment matrix entry
-            #print("Mapping "+str(curr) + " P: " + str(prev))
-            transitions[gesture_groups[curr]][gesture_groups[prev]] = transitions[gesture_groups[curr]][gesture_groups[prev]] + 1
-        
-        prev = s
 
 #for row in transitions:
 #    total = sum(row)
@@ -64,6 +68,16 @@ for col in chains:
 
 #plt.imshow(transitions, cmap=plt.cm.binary, interpolation='nearest')
 #plt.show()
+
+
+
+plt.imshow(windowed_transitions['group'].ix[0], cmap=plt.cm.binary, interpolation='nearest')
+plt.show()
+
+### Need to plot each row to a different place!
+
+
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
