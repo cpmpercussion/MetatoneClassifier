d4d3e195d5bbc05f64792560cf6ae3a47acf5200
cpmpercussion <cpm@charlesmartin.com.au>
2014-07-02 17:19:56 +1000

    made a start for the agent generator

---
+++ MetatoneAgentGen/MetatoneAgentGen.py
@@ -0,0 +1,3 @@
+## TODO: copy in relevant stuff from the MetatoneClassifier.
+## Change timer so that every 1s it generates a new gesture for each player rather than classifies one.
+## Otherwise -- everything else the same!
