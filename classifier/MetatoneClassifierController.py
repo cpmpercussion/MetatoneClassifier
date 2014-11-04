b54f41b3b4d39402c9a059be2ccc32e62744d369
cpmpercussion <cpm@charlesmartin.com.au>
2014-11-04 17:30:35 +1100

    updating the GUI controller to work - almost there!

--- classifier/MetatoneClassifierController.py
+++ classifier/MetatoneClassifierController.py
@@ -1,6 +1,7 @@
 from Cocoa import *
 from Foundation import NSObject
 import metatoneClassifier
+import threading,time
 
 class MetatoneClassifierController(NSWindowController):
     ensembleTextField = objc.IBOutlet()
@@ -8,32 +9,56 @@ class MetatoneClassifierController(NSWindowController):
 
     def windowDidLoad(self):
         NSWindowController.windowDidLoad(self)
-        ## do the stuff
-        ## init the local objects
         print("Window Loaded")
-        self.lastGestureClasses = ""
-        self.lastPerformanceState = ""
+        self.lastGestureClasses = "1 2 3 4"
+        self.lastPerformanceState = "bla bla bla"
 
     @objc.IBAction
     def startPerformance_(self,sender):
-        print("Starting Performance!")
-        ## do stuff
+        print("Starting Threaded Classification!")
+        self.classificationThread = threading.Thread(target=self.classifyForever,name="Classification-Thread")
+        self.classificationThread.start()
 
     @objc.IBAction
     def stopPerformance_(self,sender):
-        print("Stopping Performance.")
-        ## do more stuff.
+        print("Stopping Classification.")
+        self.stop_classifying()
+        self.classificationThread.join(2)
 
     def updateDisplay(self):
-        self.counterTextField.setStringValue_(self.count)
         self.ensembleTextField.setStringValue_(self.lastGestureClasses)
+        self.performanceStateTextField.setStringValue_(self.lastPerformanceState)
+
+    def classifyForever(self):
+        self.classifying = True
+        while (self.classifying):
+            try:
+                time.sleep(1)
+                metatoneClassifier.classifyPerformance()
+                metatoneClassifier.trim_touch_messages()
+            except:
+                print("Couldn't Perform Classification - Exception.")
+            # do the thing
+
+    def stop_classifying(self):
+        self.classifying = False
+
 
 if __name__ == "__main__":
     app = NSApplication.sharedApplication()
     
     viewController = MetatoneClassifierController.alloc().initWithWindowNibName_("MetatoneClassifierWindow")
+    print("Setting up the classifier")
+    metatoneClassifier.findReceiveAddress()
+    metatoneClassifier.startOscServer()
+    metatoneClassifier.load_classifier()
+    metatoneClassifier.startLog()
+
+
+
     viewController.showWindow_(viewController)
     print("Window Should be showing.")
+    viewController.updateDisplay()
 
     # Bring app to top
     NSApp.activateIgnoringOtherApps_(True)
