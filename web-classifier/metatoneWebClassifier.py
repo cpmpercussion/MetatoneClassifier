4746b9a35381b7e49fad4224207cf247a50cd2bd
cpmpercussion <cpm@charlesmartin.com.au>
2015-02-24 15:07:18 +1100

    working version of webserver classifier

--- web-classifier/metatoneWebClassifier.py
+++ web-classifier/metatoneWebClassifier.py
@@ -1,5 +1,6 @@
 import logging
-# import metatoneClassifier
+import metatoneClassifier
+import threading
 import tornado.escape
 import tornado.ioloop
 import tornado.options
@@ -13,6 +14,11 @@ from datetime import datetime
 from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
+define("name", default='MetatoneWebProc', help="name for webserver application", type=str)
+
+FAKE_OSC_IP_ADDRESS = '127.0.0.1'
+FAKE_OSC_PORT = 9999
+FAKE_OSC_SOURCE = (FAKE_OSC_IP_ADDRESS,FAKE_OSC_PORT)
 
 #logger = logging.getLogger('gateway')
 
@@ -20,7 +26,7 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
-            (r"/classifier", ClassifierHandler),
+            (r"/classifier", MetatoneAppConnectionHandler),
         ]
         settings = dict(
             cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
@@ -33,145 +39,111 @@ class Application(tornado.web.Application):
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
-        # , messages=ChatSocketHandler.cache)
 
 connections = set()
 clients = dict()
 
-def ProcessMetatoneMessageString(handler,time,packet):
-    #something
-    # ought to wrap this stuff in exception handling.
+def processMetatoneMessageString(handler,time,packet):
     message = OSC.decodeOSC(packet)
-    #print ('Decoded Message: {}'.format(repr(message)))
     try:
         if "/metatone/touch/ended" in message[0]:
-            # touch ended
-            print("Got a touch ended message.")
+            metatoneClassifier.touch_ended_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
         elif "/metatone/touch" in message[0]:
-            print("Got a touch message.")
-            # touch!
+            # def touch_handler(addr, tags, stuff, source):
+            metatoneClassifier.touch_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
         elif "/metatone/switch" in message[0]:
-            print("Got a switch message.")
-            #switch
+            metatoneClassifier.switch_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
         elif "/metatone/online" in message[0]:
-            print("Got an online message.")
-            msg = OSC.OSCMessage("/metatone/classifier/hello")
-            packet = msg.getBinary()
-            handler.write_message(packet,binary=True)
+            metatoneClassifier.onlineoffline_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            handler.sendOSC("/metatone/classifier/hello",[])
             handler.deviceID = message[2]
             handler.app = message[3]
-            # online! add to something.
         elif "/metatone/offline" in message[0]:
-            print("Got an offline message.")
-            # offline
+            metatoneClassifier.onlineoffline_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
         elif "/metatone/acceleration" in message[0]:
-            print("Got an accel message.")
-            # accel.
+            metatoneClassifier.accel_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
         elif "/metatone/app" in message[0]:
-            print("Got an app message.")
-            # app message.
+            metatoneClassifier.metatone_app_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+        elif "/metatone/targetgesture" in message[0]:
+            metatoneClassifier.target_gesture_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
         else:
             print("Got an unknown message! Address was: " + message[0])
             print u'Raw Message Data: {}'.format(packet)
     except():
         print("Message did not decode to a non-empty list.")
-        
 
 
-# # def classify():
-# #     # calculate gestures
-# #     # calculate performance state
-# #     # performance evemt?
-# #     for c in connections:
-# #         # send gesture to c
-# #         # send performance state to c
-# #         if performanceEvent:
-# #             #send performance event to c
+def sendOSCToAllClients(address,arguments):
+    print("Sending OSC to All Clients: " + repr(address) + repr(arguments))
+    for connection in connections:
+        connection.sendOSC(address,arguments)
 
-class ClassifierHandler(tornado.websocket.WebSocketHandler):
+def sendOSCToIndividualClients(address,device_to_arg_dict):
+    print("Sending OSC to Individual Clients: " + repr(address) + repr(device_to_arg_dict))
+    for connection in connections:
+        connection.sendOSC(address,[connection.deviceID,device_to_arg_dict[connection.deviceID]])
+
+##############################################
+
+class MetatoneAppConnectionHandler(tornado.websocket.WebSocketHandler):
     deviceID = ''
     app = ''
 
     def open(self):
         print("Client opened WebSocket")
         connections.add(self)
-        logging.info(datetime.now().isoformat() + "Classifier Opened.")
+        logging.info(datetime.now().isoformat() + " Connection Opened.")
         print("Connections" + repr(connections))
 
     def on_message(self,message):
         time = datetime.now()
-        logging.info(time.isoformat() + " " + message)
-        ProcessMetatoneMessageString(time,message)
-        # self.write_message(message)
-        #rint(message)
-        # print("Attempting to return foo bar message.")
-        msg = OSC.OSCMessage("/foo/bar")
-        # msg.extend(['string argument',1,2,3.14])
-        packet = msg.getBinary()
-        print(u'Sending Message: {}'.format(packet))
-        # # OSC.decodeOSC(packet)
-        self.write_message(packet,binary=True)
-        
+        processMetatoneMessageString(self,time,message)
+            
     def on_close(self):
         print("Client closed WebSocket")
-        logging.info(datetime.now().isoformat() + " Classifier Closed.")
+        logging.info(datetime.now().isoformat() + " Connection Closed.")
         connections.remove(self)
 
-    # waiters = set()
-    # cache = []
-    # cache_size = 200
+    def sendOSC(self,address,arguments):
+        msg = OSC.OSCMessage(address)
+        msg.extend(arguments)
+        packet = msg.getBinary()
+        try:
+            self.write_message(packet,binary=True)
+        except:
+            logging.error("Error sending OSC message to client", exc_info=True)
 
-    # def allow_draft76(self):
-    #     # for iOS 5.0 Safari
-    #     return True
-
-    # def open(self):
-    #     ChatSocketHandler.waiters.add(self)
-
-    # def on_close(self):
-    #     ChatSocketHandler.waiters.remove(self)
-
-    # @classmethod
-    # def update_cache(cls, chat):
-    #     cls.cache.append(chat)
-    #     if len(cls.cache) > cls.cache_size:
-    #         cls.cache = cls.cache[-cls.cache_size:]
-
-    # @classmethod
-    # def send_updates(cls, chat):
-    #     logging.info("sending message to %d waiters", len(cls.waiters))
-    #     for waiter in cls.waiters:
-    #         try:
-    #             waiter.write_message(chat)
-    #         except:
-    #             logging.error("Error sending message", exc_info=True)
-
-    # def on_message(self, message):
-    #     logging.info("got message %r", message)
-    #     parsed = tornado.escape.json_decode(message)
-    #     chat = {
-    #         "id": str(uuid.uuid4()),
-    #         "body": parsed["body"],
-    #         }
-    #     chat["html"] = tornado.escape.to_basestring(
-    #         self.render_string("message.html", message=chat))
-
-    #     ChatSocketHandler.update_cache(chat)
-    #     ChatSocketHandler.send_updates(chat)
-
+##############################################
 
 def main():
     # Logging
-    global logger
-    logging_filename = datetime.now().isoformat().replace(":","-")[:19] + "-MetatoneOSCLog.log"
-    logging.basicConfig(filename="logs/"+logging_filename,level=logging.DEBUG)
-    logging.info("Logging started - " + logging_filename)
-    print ("Classifier Server Started - logging to: " + logging_filename)
+    # global logger
+    print("Loading Metatone Classifier.")
+    # metatoneClassifier.findReceiveAddress()
+    # metatoneClassifier.startOscServer()
+    metatoneClassifier.load_classifier()
+    metatoneClassifier.startLog()
+    print("Metatone Classifier Ready.")
+    logging.info("WebServer Logging started - " + metatoneClassifier.logging_filename)
+    print ("Classifier WebServer Started - logging to: " + metatoneClassifier.logging_filename)
 
     tornado.options.parse_command_line()
     app = Application()
     app.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    metatoneClassifier.name = options.name
+    metatoneClassifier.WEB_SERVER_MODE = True
+    metatoneClassifier.webserver_sendtoall_function = sendOSCToAllClients
+    metatoneClassifier.webserver_sendindividual_function = sendOSCToIndividualClients
+
+    classificationThread = threading.Thread(target=metatoneClassifier.classifyForever,name="Classification-Thread")
+    
+    try:
+        classificationThread.start()
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        print("Received Ctrl-C - Closing down.")
+        metatoneClassifier.stopClassifying()
+        print("Closed down. Bye!")
 
 
 if __name__ == "__main__":
