91ff5c3c34f7dccdf1ae5ce5481bc7a2171c90f1
cpmpercussion <cpm@charlesmartin.com.au>
2015-05-14 15:19:03 +1000

    Code cleanup for lint

--- web-classifier/metatoneWebClassifier.py
+++ web-classifier/metatoneWebClassifier.py
@@ -16,6 +16,7 @@ import OSC
 import pybonjour
 from datetime import datetime
 import random
+from __future__ import print_function
 
 define("port", default=8888, help="run on the given port", type=int)
 define("name", default='MetatoneWebProc', help="name for webserver application", type=str)
@@ -71,28 +72,28 @@ def processMetatoneMessageString(handler, time, packet):
     message = OSC.decodeOSC(packet)
     try:
         if "/metatone/touch/ended" in message[0]:
-            metatoneClassifier.touch_ended_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            metatoneClassifier.touch_ended_handler(message[0], message[1][1:], message[2:], FAKE_OSC_SOURCE)
         elif "/metatone/touch" in message[0]:
             # def touch_handler(addr, tags, stuff, source):
-            metatoneClassifier.touch_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            metatoneClassifier.touch_handler(message[0], message[1][1:], message[2:], FAKE_OSC_SOURCE)
         elif "/metatone/switch" in message[0]:
-            metatoneClassifier.switch_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            metatoneClassifier.switch_handler(message[0], message[1][1:], message[2:], FAKE_OSC_SOURCE)
         elif "/metatone/online" in message[0]:
-            metatoneClassifier.onlineoffline_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
-            handler.sendOSC("/metatone/classifier/hello",[])
+            metatoneClassifier.onlineoffline_handler(message[0], message[1][1:], message[2:], FAKE_OSC_SOURCE)
+            handler.sendOSC("/metatone/classifier/hello", [])
             handler.deviceID = message[2]
             handler.app = message[3]
         elif "/metatone/offline" in message[0]:
-            metatoneClassifier.onlineoffline_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            metatoneClassifier.onlineoffline_handler(message[0], message[1][1:], message[2:], FAKE_OSC_SOURCE)
         elif "/metatone/acceleration" in message[0]:
-            metatoneClassifier.accel_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            metatoneClassifier.accel_handler(message[0], message[1][1:], essage[2:], FAKE_OSC_SOURCE)
         elif "/metatone/app" in message[0]:
-            metatoneClassifier.metatone_app_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            metatoneClassifier.metatone_app_handler(message[0], message[1][1:], message[2:], FAKE_OSC_SOURCE)
         elif "/metatone/targetgesture" in message[0]:
-            metatoneClassifier.target_gesture_handler(message[0],message[1][1:],message[2:],FAKE_OSC_SOURCE)
+            metatoneClassifier.target_gesture_handler(message[0], message[1][1:], message[2:], FAKE_OSC_SOURCE)
         else:
             print("Got an unknown message! Address was: " + message[0])
-            print u'Raw Message Data: {}'.format(packet)
+            print(u'Raw Message Data: {}'.format(packet))
     except():
         print("Message did not decode to a non-empty list.")
 
@@ -102,32 +103,48 @@ def sendOSCToAllClients(address, arguments):
     try:
         for connection in connections:
             try:
-                connection.sendOSC(address,arguments)
+                connection.sendOSC(address, arguments)
             except:
                 print("Exception sending group message to: " + connection.deviceID)
     except:
         print("Couldn't send group message. Probably clients still joining.")
 
-def sendOSCToIndividualClients(address,device_to_arg_dict):
+def sendOSCToIndividualClients(address, device_to_arg_dict):
+    """
+    Sends an OSC formatted message with the same address to each 
+    connected client according to the dictionary of arguments.
+    """
     # print("Sending OSC to Individual Clients: " + repr(address) + ' ' + repr(device_to_arg_dict))
     for connection in connections:
         if connection.deviceID in device_to_arg_dict.keys():
             try:
-                connection.sendOSC(address,[connection.deviceID,device_to_arg_dict[connection.deviceID]])
+                connection.sendOSC(address, [connection.deviceID, device_to_arg_dict[connection.deviceID]])
             except:
                 print("Exception sending individual message to: " + connection.deviceID)
 
 def removeMetatoneAppFromClassifier(deviceID):
+    """
+    Instructs the Classifier to remove an app with a particular deviceID
+    from its list of connected sources.
+    """
     print("!!!! Removing App: " + repr(deviceID))
     metatoneClassifier.remove_source(deviceID)
 
 def clearMetatoneAppsFromClassifier():
+    """
+    Instructs the Classifier to remove ALL connected apps
+    from its list of sources.
+    """
     print("Clearing all apps from Classifier")
     metatoneClassifier.clear_all_sources()
 
 ##############################################
 
 class MetatoneAppConnectionHandler(tornado.websocket.WebSocketHandler):
+    """
+    Class for handling connection to a Metatone App. Received messages are processed and
+    sent to the classifier. Can send OSC formatted messages via the web socket connection.
+    """
     deviceID = ''
     app = ''
 
@@ -137,9 +154,9 @@ class MetatoneAppConnectionHandler(tornado.websocket.WebSocketHandler):
         logging.info(datetime.now().isoformat() + " Connection Opened.")
         # print("Connections: " + repr(connections))
 
-    def on_message(self,message):
+    def on_message(self, message):
         time = datetime.now()
-        processMetatoneMessageString(self,time,message)
+        processMetatoneMessageString(self, time, message)
             
     def on_close(self):
         print("!!!! SERVER: Client closed WebSocket: " + self.deviceID)
@@ -148,12 +165,15 @@ class MetatoneAppConnectionHandler(tornado.websocket.WebSocketHandler):
         connections.remove(self)
         print("!!!! Removal done.")
 
-    def sendOSC(self,address,arguments):
+    def sendOSC(self, address, arguments):
+        """
+        Attempts to send an OSC formatted message to the client.
+        """
         msg = OSC.OSCMessage(address)
         msg.extend(arguments)
         packet = msg.getBinary()
         try:
-            self.write_message(packet,binary=True)
+            self.write_message(packet, binary=True)
         except:
             logging.error("Error sending OSC message to client", exc_info=True)
 
@@ -179,27 +199,31 @@ def main():
     metatoneClassifier.startLog()
     print("Metatone Classifier Ready.")
     logging.info("WebServer Logging started - " + metatoneClassifier.logging_filename)
-    print ("Classifier WebServer Started - logging to: " + metatoneClassifier.logging_filename)
+    print ("Classifier WebServer Started - logging to: " 
+           + metatoneClassifier.logging_filename)
 
     tornado.options.parse_command_line()
     app = Application()
     app.listen(options.port)
     metatoneClassifier.name = options.name
     metatoneClassifier.performance_type = options.type
-    logging.info("WebServer Performance type is: " + str(options.type) + ": " + PERFORMANCE_TYPE_NAMES[options.type])
-    print("WebServer Performance type is: " + str(options.type) + ": " + PERFORMANCE_TYPE_NAMES[options.type])
-    metatoneClassifier.performance_composition = random.randint(0,100)
+    logging.info("WebServer Performance type is: " + str(options.type) 
+                 + ": " + PERFORMANCE_TYPE_NAMES[options.type])
+    print("WebServer Performance type is: " + str(options.type) 
+          + ": " + PERFORMANCE_TYPE_NAMES[options.type])
+    metatoneClassifier.performance_composition = random.randint(0, 100)
     metatoneClassifier.WEB_SERVER_MODE = True
     metatoneClassifier.webserver_sendtoall_function = sendOSCToAllClients
     metatoneClassifier.webserver_sendindividual_function = sendOSCToIndividualClients
 
-    classificationThread = threading.Thread(target=metatoneClassifier.classifyForever,name="Classification-Thread")
+    classificationThread = threading.Thread(target=metatoneClassifier.classifyForever, name="Classification-Thread")
 
     print("Starting Bonjour Service.")
-    bonjourServiceRegister = pybonjour.DNSServiceRegister(name = options.name,
-                                     regtype = METACLASSIFIER_SERVICE_TYPE,
-                                     port = options.port,
-                                     callBack = bonjour_callback)
+    bonjourServiceRegister = pybonjour.DNSServiceRegister(
+        name=options.name,
+        regtype=METACLASSIFIER_SERVICE_TYPE,
+        port=options.port,
+        callBack=bonjour_callback)
     
     try:
         classificationThread.start()
