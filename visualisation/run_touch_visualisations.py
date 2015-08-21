82c26a74417b1f6652136f677f759f95fd9df287
Charles Martin <cpm@charlesmartin.com.au>
2015-08-21 16:55:07 +1000

    script for running visualisations

---
+++ visualisation/run_touch_visualisations.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+""" copies touch files into the Processing app's path, then runs the app"""
+import os
+print("Find all the touch files.")
+app_name = "/Users/charles/src/MetatoneClassifier/visualisations/MetatoneTouchLogPlayer"
