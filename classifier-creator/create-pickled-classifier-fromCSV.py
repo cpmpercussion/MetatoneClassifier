484bd82b2f9d5180154fb251c220fd7476893bfc
Charles Martin <cpm@charlesmartin.com.au>
2016-05-20 21:11:12 +1000

    cleaning up evaluation script

--- classifier-creator/create-pickled-classifier-fromCSV.py
+++ classifier-creator/create-pickled-classifier-fromCSV.py
@@ -9,8 +9,6 @@ of ANOVA and paired t-tests to compare the different sets.
 Average confusion matrices are calculated and plotted for each set
 of data.
 """
-from datetime import timedelta
-from datetime import datetime
 import pickle
 import numpy as np
 import pandas as pd
@@ -30,9 +28,8 @@ GESTURE_CODES = { ## Int values for Gesture codes.
     'BS': 6,
     'SS': 7,
     'C': 8}
-
 ## Column names in the feature vectors:
-feature_vector_columns = ['centroid_x','centroid_y','std_x','std_y','freq','movement_freq','touchdown_freq','velocity']
+FEATURE_VECTOR_COLUMNS = ['centroid_x', 'centroid_y', 'std_x', 'std_y', 'freq', 'movement_freq', 'touchdown_freq', 'velocity']
 
 def pickleClassifier(file_name):
     """
@@ -45,9 +42,9 @@ def pickleClassifier(file_name):
     test = feature_vectors[~msk]
     # build classifier
     classifier = RandomForestClassifier(n_estimators=100, max_features=3)
-    classifier = classifier.fit(train[feature_vector_columns],train['gesture'])
+    classifier = classifier.fit(train[FEATURE_VECTOR_COLUMNS],train['gesture'])
     # score classifier
-    mean_accuracy = classifier.score(test[feature_vector_columns],test['gesture'])
+    mean_accuracy = classifier.score(test[FEATURE_VECTOR_COLUMNS],test['gesture'])
     print('Mean Accuracy:' + str(mean_accuracy) + '\n')
     # save pickled classifier
     pickle_file = open( file_name.replace('.csv','-classifier.p'), "wb" )
@@ -62,17 +59,17 @@ def evaluateClassifier(classifier,feature_vectors):
     Scores returned as a numpy array.
     """
     ## Using stratified K-Folds validation
-    vectors = np.array(feature_vectors[feature_vector_columns])
+    vectors = np.array(feature_vectors[FEATURE_VECTOR_COLUMNS])
     targets = np.array(feature_vectors['gesture'])
 
-    skf = StratifiedKFold(targets,FOLDS,shuffle=True)
+    skf = StratifiedKFold(targets, FOLDS, shuffle=True)
     scores = np.array([])
-    confusion = np.zeros((9,9))
+    confusion = np.zeros((9, 9))
     for train, test in skf:
         X_train, X_test, y_train, y_test = vectors[train], vectors[test], targets[train], targets[test]
-        classifier.fit(X_train,y_train)
-        s = classifier.score(X_test,y_test)
-        scores = np.append(scores,s)
+        classifier.fit(X_train, y_train)
+        s = classifier.score(X_test, y_test)
+        scores = np.append(scores, s)
         y_pred = classifier.predict(X_test)
         for x in range(len(y_test)):
             confusion[y_test[x]][y_pred[x]] = confusion[y_test[x]][y_pred[x]] + 1
@@ -80,11 +77,14 @@ def evaluateClassifier(classifier,feature_vectors):
     return scores, confusion
 
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
-    plt.figure(figsize=(5.5,4.5),dpi=300)
+    """
+    Plots and saves the confusion matrix cm.
+    """
+    plt.figure(figsize=(5.5, 4.5), dpi=300)
     plt.title(title)
-    plt.imshow(cm, cmap=plt.cm.Blues, interpolation='nearest')
+    plt.imshow(cm, cmap=cmap, interpolation='nearest')
     plt.colorbar() # shows the legend
-    labels =  ['N','FT','ST','FS','FSA','VSS','BS','SS','C']
+    labels = ['N', 'FT', 'ST', 'FS', 'FSA', 'VSS', 'BS', 'SS', 'C']
     plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8], labels)
     plt.yticks([0, 1, 2, 3, 4, 5, 6, 7, 8], labels)
     plt.tight_layout()
@@ -98,13 +98,13 @@ data20130701_5s = '2013-07-01-TrainingData.csv' # 5s windowed data
 data20130701_1s = '2013-07-01-TrainingData1s.csv' # 1s windowed data
 data20141212_cpm = '2014-12-12T12-05-53-GestureTargetLog-CPM-FeatureVectors.csv' # new processing collected data
 
-short_names = {data20130701_5s:"Studio 5s Window",data20130701_1s:"Studio 1s Window",data20141212_cpm:"Formal Procedure"}
+short_names = {data20130701_5s:"Studio 5s Window", data20130701_1s:"Studio 1s Window", data20141212_cpm:"Formal Procedure"}
 
-data = [data20130701_5s,data20130701_1s,data20141212_cpm]
+data = [data20130701_5s, data20130701_1s, data20141212_cpm]
 data_scores = []
 data_confusion_matrices = []
 tests = []
-confusions = {data20130701_5s:np.zeros((9,9)),data20130701_1s:np.zeros((9,9)),data20141212_cpm:np.zeros((9,9))}
+confusions = {data20130701_5s:np.zeros((9,9)), data20130701_1s:np.zeros((9,9)), data20141212_cpm:np.zeros((9,9))}
 output = pd.DataFrame(index=pd.Series(range(100)))
 
 print("Testing Training Data...")
